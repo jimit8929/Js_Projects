6d28242c969e55f2a95661dab8a62061cbc8d653
jimit8929 <jimit8929@gmail.com>
2025-07-03 16:25:36 +0530

    Kanban Board

--- 02_Task_Management/tailwind.config.js
+++ 02_Task_Management/tailwind.config.js
@@ -1,1 +1,12 @@
 darkMode: 'class'
+
+module.exports = {
+  content: [
+    "./*.html",
+    "./scripts/**/*.js",
+    "../02_Task_Management/styles/index.css", // if you import components there
+  ],
+  theme: {
+  },
+  plugins: [],
+};
