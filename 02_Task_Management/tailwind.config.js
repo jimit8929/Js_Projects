bfc4b2ec5c12be136e623f754c95abec49e0bfd7
jimit8929 <jimit8929@gmail.com>
2025-07-03 16:34:55 +0530

    Task_Management-Kanban Board

--- 02_Task_Management/tailwind.config.js
+++ 02_Task_Management/tailwind.config.js
@@ -1,12 +1,14 @@
-darkMode: 'class'
-
 module.exports = {
+  darkMode: 'class',            // ← you already have this
   content: [
-    "./*.html",
-    "./scripts/**/*.js",
-    "../02_Task_Management/styles/index.css", // if you import components there
+    './*.html',                 // any top‑level HTML (e.g. index.html)
+    './02_Task_Management/**/*.html',    // all HTML in your project folder
+    './02_Task_Management/scripts/**/*.js', // your JS entrypoints
   ],
   theme: {
+    extend: {
+      // (optional) anything custom you had in index.css
+    }
   },
   plugins: [],
-};
+}
